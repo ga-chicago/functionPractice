5ed463519c93d3cec9841c1f828c3495457c137c
Bridjet Mendyuk <bjmendy@Bridjets-MBP.attlocal.net>
2017-02-07 23:57:02 -0600

    finished homework

---
+++ functionPractice.js
@@ -0,0 +1,90 @@
+//#1//
+for (var i = 0; i <= 10; i++){
+	
+		console.log(i * 9)
+	
+}
+
+//#2// //#3//
+function assignGrade(number){
+	if(number >= 90){
+		console.log("you got an A");
+	}
+	}else if(number >= 80){
+		console.log("you got a B");
+	}else if(number >= 70){
+		console.log("you got a C");
+	}else if(number >= 60){
+		console.log("you got a D");
+	}else {
+	console.log("you got an F")
+}
+}
+}
+
+//#4//#5//#6//
+
+function tellFortune(numberOfChildren, partnersName, location, job){
+
+	console.log('you will be a' + job + 'in' + location + 'married to' + partnersName + 'with' + numberOfChildren + 'kids');
+}
+
+//#7//
+function tellFortune(futureHouse, newDogName, divorceDate){
+}
+
+//#8//
+coolArray = [1, 2, 3, 4, 5];
+outputArray = [2, 4];
+function spillNumbers(secondLowest, secondGreatest){
+	var coolArray = array.sort();
+	coolArray.push(outputArray[2], coolArray[coolArray.length]);
+	return outputArray;
+}
+
+///#9//
+function acceptIt(string, letter){
+	var letter_Count = 0;
+	for (var position = 0; position < str.length; position++)
+	if(str.charAt(position) == letter)
+	{
+		letter_Count += 1;
+	}
+}
+	return letter_Count;
+}
+console.log(char_count('whose going to the bathroom', 'o'))
+}
+
+//#10//
+function countryNames(["Australia", "Germany", "United States of America"]){
+
+	Longest_Country_Name(country_name)
+  {
+  return country_name.reduce(function(lname, country) 
+  {
+    return lname.length > country.length ? lname : country;
+  }, 
+"");
+}
+console.log(Longest_Country_Name(["Australia", "Germany", "United States of America"]));
+}
+
+//#11//
+String.prototype.reverse = function() {
+	return this.split('').reverse().join('');
+};
+var n = 50697;
+alert(('' + n).reverse());
+
+
+
+
+
+
+
+//#11//
+
+
+
+
