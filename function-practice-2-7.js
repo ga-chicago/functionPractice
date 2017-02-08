5f552f2ee776328ec6e68ad4143498a313e9ba2d
Michael Abood <Schroeder@Mikes-Macbook.local>
2017-02-08 00:17:06 -0600

    submitted correct file

---
+++ function-practice-2-7.js
@@ -0,0 +1,90 @@
+//number 1
+
+for (i = 0; i <= 10; i++){
+	console.log(i * 9);
+}
+
+//number 2 && 3
+
+function assignGrade(numScore){
+	if(numScore >= 90){
+		console.log("A");
+	}
+	else if(numScore >= 80){
+		console.log("B");
+	}
+	else if(numScore >= 70){
+		console.log("C");
+	}
+	else if(numScore >= 60){
+		console.log("D");
+	}
+	else{
+		console.log("F");
+	}
+
+}
+
+//number 4
+
+function tellFortune(jobTitle, geoLoco, partName, numChild){
+	console.log("You will be a " + jobTitle + " in " + geoLoco + ", and married to " + partName + " with " + numChild + " kids.");
+
+}
+
+tellFortune("Nuclear Saftey Inspector","Springfield","Marge",3)
+tellFortune("Co-owner of a bar","Philladelphia","Maureen Ponderosa",0)
+tellFortune("Meth dealer/Chemistry teacher","Albuquerque, NM ","Skylar White",2)
+
+
+
+//number 5
+
+arrNum = [2, 8, 5, 3]; 
+var newArray []
+
+function highLowNum(arrNum){
+	for (i=0; i < arrNum.length; i++){
+		var newArray = (arrNum[i].push);
+		// var newArray.push(arrNum); 
+		// .sort
+
+		console.log(newArray);
+
+
+	}
+}
+
+
+//number 6
+
+function numOccur(string,letter) {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
