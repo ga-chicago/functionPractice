a3b8c8e21bda8c8d88cb7d2e4edc0cc10b215119
aaronsato <2asato@gmail.com>
2017-06-06 23:56:21 -0700

    function hw

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,4 @@
+//1
 var multiplier = 9;
 for (var i = 0; i <= 10; i++) {
 	var result = multiplier * i;
@@ -5,6 +6,7 @@ for (var i = 0; i <= 10; i++) {
 }
 
 
+//2
 function assignGrade(score) {
     if (score > 90) {
         return score + ' returns A';
@@ -19,22 +21,27 @@ function assignGrade(score) {
     }
 }
 
+
+//3
 for (var i = 60; i <= 100; i++){
 	console.log('For ' + i + ', you got a ' + assignGrade(i));
 }
 
 
+//4,5,6
 function tellFortune(numChildren, partner, location, job){
 	var future = 'You will be a ' + job + ' in ' + location + ' and married to ' + partner + ' with ' + numChildren + ' kids.';
 		console.log(future);
 }
-	
+
+
+//7
 	tellFortune(2, 'Joni', 'Seattle', 'web designer');
 	tellFortune(3, 'Mary', 'Phoenix', 'surgeon');
 	tellFortune(5, 'Kim', 'Chicago', 'Rapper');
 
 
-
+//9
 function characters(string, letter){
 	var letterCount = 0;
 	for (var position = 0; position < string.length; position++){
@@ -47,7 +54,7 @@ function characters(string, letter){
 console.log(characters('Arbys we have the meat', 'e'));
 
 
-
+//10
 function longestCountryName(name){
 	return name.reduce(function(longName, country){
 		return longName.length > country.length ? longName : country;
@@ -57,13 +64,7 @@ function longestCountryName(name){
 console.log(longestCountryName(['Australia', 'Philippines', 'Japan']));
 
 
-
-
-
-
-
-
-
+//11
 function reverseNumber(x){
 	x = x + "";
 	return x.split("").reverse("").join("");
