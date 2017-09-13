1a0223a602b5ff8b2ed4ce6bc86448142f96d3cc
Myka Martinez <michaelmartinez-mann@Yin.local>
2017-09-13 16:43:29 -0500

    problem 1 solved

---
+++ functionPractice.js
@@ -0,0 +1,21 @@
+// 1.
+
+let product;
+
+for (let i = 0; i <= 10; i++) {
+ 	product = i * 9;
+ 	console.log(product);
+}
+
+// 1. Bonus
+
+let table
+
+for (let i = 0; i <= 10; i++) {
+ 	for (m = 1; m <= 10; m++){
+ 		if (i > 0) {
+		table = i * m
+		console.log(i, "*", m, "=", table)
+		}
+ 	}
+ }
