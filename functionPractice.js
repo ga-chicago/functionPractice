44178394df0c42e7e20c3e31a99dffd9da5ac623
Andrew Nagle <andrew@Andrews-MacBook-Pro.local>
2017-09-13 18:16:25 -0500

    finished problem 2

--- functionPractice.js
+++ functionPractice.js
@@ -3,3 +3,18 @@ for (let i = 0; i <=10; i++){
 	const x = i * 9
 	console.log(x);
 };
+// problem 2
+const assignGrade = (i) => {
+	if (i >=90) {
+		console.log("A")
+	} else if (i >= 80){
+		console.log("B")
+	} else if (i >= 70){
+		console.log("C")
+	} else if (i >=60){
+		console.log("D")
+	} else{
+		console.log("F")
+	}
+};
+
