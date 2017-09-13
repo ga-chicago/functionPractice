9207a5fcecf3e37501d453c4839d80005c328ecd
Thomas Murphy <thomasmurphy@Thomass-MacBook-Air.local>
2017-09-13 16:33:12 -0500

    I completed problem 1

---
+++ 09132017hw.js
@@ -0,0 +1,13 @@
+//question 1
+
+for (i = 0; i <= 10; i++) {
+	console.log(i * 9);
+}
+
+//question 1 bonus
+
+for (i = 0; i <= 10; i++) {
+	for (z = 1; z <= 10; z++) {
+		console.log(i * z);
+	}
+}
