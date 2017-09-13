092d2f3d97ad2d43e4a3c004f1b345bc4526e7b0
Thomas Murphy <thomasmurphy@Thomass-MacBook-Air.local>
2017-09-13 16:38:47 -0500

    I completed problem 2

--- 09132017hw.js
+++ 09132017hw.js
@@ -11,3 +11,19 @@ for (i = 0; i <= 10; i++) {
 		console.log(i * z);
 	}
 }
+
+//question 2
+
+const assignGrade = (x) => {
+	if (x > 89) {
+		return console.log('A');
+	} else if (x > 79) {
+		return console.log('B');
+	} else if (x > 69) {
+		return console.log('C');
+	} else if (x > 59) {
+		return console.log('D');
+	} else {
+		return console.log('F');
+	}
+};
