9a965beaf366944f9652fac0145874cafb7ada7a
Giovanni Perez <giovanniperez@Giovannis-MacBook-Pro.local>
2017-09-13 17:02:55 -0500

    is #1 really that easy?

---
+++ assignment1.js
@@ -0,0 +1,73 @@
+//1- cant display full function
+for( i = 0; i < 11; i++) {
+
+	console.log(i + ' * 9' + ' = ' + i *9)
+}
+
+
+
+//2
+
+const assignGrade = (grade) => {
+	if(grade >= 89) {
+		console.log("Congrats, you received an A!")
+
+	} else if(grade >= 89) {
+		console.log("Congrats, you received a B!")
+	} else if(grade >= 89) {
+		console.log("You received a C!")
+	} else if(grade >= 89) {
+		console.log("Dude, you received a D.")
+	} else {
+		console.log("Congrats, you received a B!")
+	} 
+}
+
+
+
+//3
+
+
+
+//4
+
+
+
+
+//5
+
+
+
+
+
+//6
+
+
+
+
+//7
+
+
+
+//8
+
+
+
+
+
+//9
+
+
+
+
+
+
+//10
+
+
+
+
+
+
+//11
+
