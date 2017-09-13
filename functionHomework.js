ae179151ffc398a1cb440c2fb0e204fcd8361b80
matbakutis <31048002+matbakutis@users.noreply.github.com>
2017-09-13 17:07:51 -0500

    Finished the third problem

--- functionHomework.js
+++ functionHomework.js
@@ -22,20 +22,8 @@ const assignGrade = (score) => {
 	};
 };
 
-// Mess Up
-const assignGrade = (score) => {
-	if (score <= 100 && score >= 90){
-		return ("Your score of " + score + " gives you an 'A'.");
-	}else if (score < 90 && score >= 80){
-		return ("Your score of " + score + " gives you an 'B'.");
-	}else if (score < 100 && score >= 70){
-		return ("Your score of " + score + " gives you an 'C'.");
-	}else if (score < 100 && score >= 60){
-		return ("Your score of " + score + " gives you an 'D'.");
-	}else if (score < 60){
-		return ("Your score of " + score + " gives you an 'F'.");
-	}else if (score > 100){
-		console.log("Extra credit was not given, therefore your score of " + score + " is not valid.");
-	};
+// Number 3
+for (i = 60; i <= 100; i++){
+	console.log("For your score of " + i + " you got the letter grade of " + assignGrade(i) + ".");
 };
 
