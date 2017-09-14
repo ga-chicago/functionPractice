c8689b50340bd157b245a09762150c49e75051b0
tom.louis.murphy <tom.louis.murphy@gmail.com>
2017-09-13 23:51:14 -0500

    I completed problem 8

--- 09132017hw.js
+++ 09132017hw.js
@@ -85,3 +85,13 @@ const longestCountryName = (countries) => {
 }
 
 //question 8
+
+const reverse = (n) => {
+	let r = 0; 
+	while (n!=0) {
+		r *= 10;
+		r += n % 10;
+		n = Math.floor(n / 10);
+	}
+	return r;
+}
