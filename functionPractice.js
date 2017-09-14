21746fa2d8451527c63fdbc3ebb9938a375f1100
Myka Martinez <michaelmartinez-mann@Yin.local>
2017-09-13 23:52:40 -0500

    problem 4 solved

--- functionPractice.js
+++ functionPractice.js
@@ -48,7 +48,16 @@ for (let s = 60; s <= 100; s++) {
 	assignGrade(s);
 }
 
+// 4.
 
+const tellFortune = (numChild, partName, location, job) => {
+	return "You will be a " + job + " in " + location + ", and married to " + 
+	partName + " with " + numChild + " kids."
+}
+
+tellFortune(3, "Sally", "Denver", "botonist")
+tellFortune(1, "Tara", "San Francisco", "writer")
+tellFortune(3, "Lacy", "Cleaveland", "chef")
 
 
 
