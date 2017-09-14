5ca3998e9fdf90d678a57dd5bf3f02713d5c1e8d
sjgraffjr <sjgraffjr@gmail.com>
2017-09-13 21:25:04 -0500

    solution to problem 2

--- StephenGraffFunctionPractice.js
+++ StephenGraffFunctionPractice.js
@@ -1,4 +1,23 @@
+
+//1.
 for(let i = 0; i < 10; i++){
 	console.log(i + " * 9 = " + i * 9)
 }
 
+
+
+//2.
+const assignGrade = (x) => {
+ if(x >= 90){
+		return "A"
+	}else if (x >= 80){
+	 	return "B"
+	}else if (x >= 70){
+	 	return "C"
+	}else if (x >= 60){
+	 	return "D"
+	}else {
+	 	return "F"
+	 }
+
+};
