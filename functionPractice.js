d71accf30de72cbd6b1001df7c3734f99e501307
Dennis Samuel Sheehan <ssheehan1210@yahoo.com>
2017-09-13 23:34:57 -0500

    I completed problem 8

--- functionPractice.js
+++ functionPractice.js
@@ -113,3 +113,9 @@ longestCountryName(["Australia", "Germany", "United States of America"]);
 // Write a JavaScript function that reverse a number. Example :
 // x = 32243; Expected Output : 34223
 
+const numberReverse = (x) => {
+	let stringOfNumber = x.toString().split('').reverse().join('');
+	let revNumber = parseInt(stringOfNumber);
+	return revNumber;
+};
+numberReverse(32243);
